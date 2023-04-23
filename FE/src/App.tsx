455df377cb0773cff4e05781cbcce9ebc906b6fb
Artem <varivonchikartem@gmail.com>
2023-04-23 13:15:30 -0700

    Setup LoginPage and LoginForm

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Registration } from "./pages/Registration";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="registration" element={<Registration />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   );
 }
