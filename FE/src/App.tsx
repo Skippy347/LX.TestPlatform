35292f00898416126b9f36b1c506c91916f90b03
Artem Varivonchik <105917366+Skippy347@users.noreply.github.com>
2023-04-23 07:15:28 -0700

    Revert "Create a HomePage and a RegistrationPage"

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Registration } from "./pages/Registration";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="registration" element={<Registration />} />
-    </Routes>
-  );
+	return <div>Test</div>;
 }
 
 export default App;
